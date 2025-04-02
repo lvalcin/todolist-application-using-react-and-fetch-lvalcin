d5b21b6b5d7cd1c5f5b32543101f4b04ad4c054d
Lovnyth Valcin <lovnyth@gmail.com>
2025-04-02 02:32:43 +0000

    updated button styles and along with  span to chnage colors when hover

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -102,12 +102,12 @@ const Home = () => {
 					<div className= "container border p-2 fs-3 text fw-light w-75">
 						<ul className="list-inline">
 							<li>{item.label}</li>
-						<span onClick={() => deleteToDo(item.id)}>
-							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className=" bi bi-trash" viewBox="0 0 16 16">
-							<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
-							<path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
-							</svg>
-						</span>
+							<span onClick={() => deleteToDo(item.id)}>
+								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className=" bi bi-trash" viewBox="0 0 16 16">
+								<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+								<path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+								</svg>
+							</span>
 						</ul>
 					</div>)
 				})
@@ -126,7 +126,8 @@ const Home = () => {
           		if (inputValue.trim() !== "") {
             	setListArray((prevList) => [prevList, todoObject]);	
             	setInputValue("");}
-			}}>
+			}} 
+			>
 			Add To List
 			</button>
 
