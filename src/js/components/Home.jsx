2cec7d893230acd6a58cf56a8a5f8f7c3674643f
Lovnyth Valcin <lovnyth@gmail.com>
2025-04-02 02:07:39 +0000

    delete button updated to trash icone, styling done to include adding margin to page, putting color and updating font

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -88,21 +88,26 @@ const Home = () => {
 	useEffect(()=>{
 		getToDos();
 		createUser();
-		// addToDo()
+		addToDo()
 	},[])	
 
 	return (
-		<div className="text-center">
+		<div className="text-center m-5 ">
 			<h1>To-Do List</h1>
 			<ul>
 
             {listArray.map(
 				(item)=>{
 					return (
-					<div className= "container border p-2 fs-3 text fw-light">
+					<div className= "container border p-2 fs-3 text fw-light w-75">
 						<ul className="list-inline">
 							<li>{item.label}</li>
-						<button onClick={() => deleteToDo(item.id)}>Delete</button>
+						<span onClick={() => deleteToDo(item.id)}>
+							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className=" bi bi-trash" viewBox="0 0 16 16">
+							<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+							<path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+							</svg>
+						</span>
 						</ul>
 					</div>)
 				})
